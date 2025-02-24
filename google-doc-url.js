3b683dcb21beb86a45f63bb736cd31896be52115
chriskeeve <157845882+chriskeeve@users.noreply.github.com>
2025-02-24 17:11:00 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -4,4 +4,4 @@ var googleDocURL = 'https://docs.google.com/spreadsheets/d/1WRONuvOQOSO4bEGyghJE
 // insert your own Google Sheets API key from https://console.developers.google.com
 // new API AIzaSyAIiuTJG1HdcQr_JhGfWAQq7DmonoUcZoY 
 // original API AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyAIiuTJG1HdcQr_JhGfWAQq7DmonoUcZoY';
